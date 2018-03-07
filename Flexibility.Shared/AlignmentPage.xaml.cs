1b529e9208cd4ce344a62ee7d27f8764c61f7586
David Ortinau <david.ortinau@microsoft.com>
2018-03-06 20:46:18 -0800

    alignment page. Login VSM. Updated to latest nightly build.

---
+++ Flexibility.Shared/AlignmentPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Flexibility.Shared
+{
+    public partial class AlignmentPage : ContentPage
+    {
+        public AlignmentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Flexibility.Shared/LoginPage.xaml.cs
+++ Flexibility.Shared/LoginPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Utils;
 using Xamarin.Forms;
+using Xamarin.Forms.Media;
 
 namespace Flexibility.Shared
 {
@@ -9,24 +10,77 @@ namespace Flexibility.Shared
         public LoginPage()
         {
             InitializeComponent();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var stateGroup = new VisualStateGroup
+            {
+                Name = "StrengthStates",
+                TargetType = typeof(Label)
+            };
+
+            stateGroup.States.Add(CreateState("Blank", "", Color.White));
+            stateGroup.States.Add(CreateState("VeryWeak", "\uf023", Color.Red));
+            stateGroup.States.Add(CreateState("Weak", "\uf023 \uf023", Color.Orange));
+            stateGroup.States.Add(CreateState("Medium", "\uf023 \uf023 \uf023", Color.Yellow));
+            stateGroup.States.Add(CreateState("String", "\uf023 \uf023 \uf023 \uf023", Color.Green));
+            stateGroup.States.Add(CreateState("VeryStrong", "\uf023 \uf023 \uf023 \uf023 \uf023", Color.Green));
+
+            VisualStateManager.SetVisualStateGroups(this.StrengthIndicator, new VisualStateGroupList { stateGroup });
+        }
+
+        static VisualState CreateState(string strength, string text, Color color)
+        {
+            var textSetter = new Setter { Value = text, Property = Label.TextProperty };
+            var colorSetter = new Setter { Value = color, Property = Label.TextColorProperty };
+
+            return new VisualState
+            {
+                Name = strength,
+                TargetType = typeof(Label),
+                Setters = { textSetter, colorSetter }
+            };
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            if(string.IsNullOrEmpty(UserNameEntry.Text) || UserNameEntry.Text.Length < 5)
+            var isValid = true;
+
+            if (string.IsNullOrEmpty(UserNameEntry.Text) || UserNameEntry.Text.Length < 5)
+            {
                 VisualStateManager.GoToState(UserNameEntry, "Invalid");
+                isValid = false;
+            }
 
             if (string.IsNullOrEmpty(PasswordEntry.Text) || PasswordEntry.Text.Length < 5)
+            {
                 VisualStateManager.GoToState(PasswordEntry, "Invalid");
+                isValid = false;
+            }
 
-            DisplayAlert("Welcome to Visual State Manager", "", "Thanks!");
+            if (isValid)
+                DisplayAlert("Welcome to Visual State Manager", "", "Thanks!");
         }
 
         void Handle_TextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
         {
             var strength = PasswordAdvisor.CheckStrength(e.NewTextValue);
             var strengthName = Enum.GetName(typeof(PasswordScore), strength);
-            VisualStateManager.GoToState(StrengthIndicator, strengthName);
+            VisualStateManager.GoToState(this.StrengthIndicator, strengthName);
+        }
+
+        private string strength;
+
+        public string Strength { 
+            get => strength; 
+            set { 
+                strength = value;
+                OnPropertyChanged(nameof(Strength));
+            } 
         }
     }
 }
